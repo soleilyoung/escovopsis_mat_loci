3ef4ee37e116382217c09cf0c0f066b1306fa189
Soleil Young <43007078+soleilyoung@users.noreply.github.com>
2026-01-13 15:24:58 -0800

    Update make_gene_trees.py
    removed erroneous #s

--- make_gene_trees.py
+++ make_gene_trees.py
@@ -21,11 +21,11 @@ with open(gene_list, 'r') as gene_f:
 
 for gene in genes:
     busco_seq = str("../busco_seq/"+gene+".fasta")
-    #subprocess.call("sed -i'' -e 's/:/_/' "+busco_seq, shell=True)
-    #subprocess.call("mafft --auto "+busco_seq+" > ../aligned_seqs/"+gene+"_aln.fasta", shell=True)
-    #trim = subprocess.Popen(["trimal","-automated1","-in","../aligned_seqs/"+gene+"_aln.fasta",
+    subprocess.call("sed -i'' -e 's/:/_/' "+busco_seq, shell=True)
+    subprocess.call("mafft --auto "+busco_seq+" > ../aligned_seqs/"+gene+"_aln.fasta", shell=True)
+    trim = subprocess.Popen(["trimal","-automated1","-in","../aligned_seqs/"+gene+"_aln.fasta",
                              #"-out","../aligned_seqs/"+gene+"_trim.fasta"])
-    #trim.communicate()
+    trim.communicate()
     iqtree2 = subprocess.Popen(["iqtree2","--boot-trees","-T","3","-s",
                                 "../aligned_seqs/"+gene+"_trim_fixed.fasta","-B","1000"])
     iqtree2.communicate()
